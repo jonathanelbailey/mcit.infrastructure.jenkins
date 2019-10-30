f6f6953d6884bbcbb9d93a1383f636f9ad4a3714
Jonathan Bailey <bailey.jonathan@hotmail.com>
2019-10-30 01:21:29 -0500

    updated chart var

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -4,8 +4,9 @@ name=jenkins
 namespace=$name
 helm_chart=stable/${name}
 svc_name=service/${name}
+chart_values=${name}.yaml
 ip_address=$1
 port=8080
-microk8s.helm install --name $name --namespace $namespace --set $helm_chart
+microk8s.helm install --name $name --namespace $namespace -f $chart_values $helm_chart
 printf $(microk8s.kubectl get secret --namespace $namespace $name -o jsonpath="{.data.${name}-admin-password}" | base64 --decode);echo
 # microk8s.kubectl port-forward $svc_name -n $name --address $ip_address 8080 &

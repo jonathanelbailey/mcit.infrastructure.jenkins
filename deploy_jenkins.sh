f34c10b11548f0919bc6c81737f5c1999c1cdd95
Jonathan Bailey <bailey.jonathan@hotmail.com>
2019-10-30 01:35:26 -0500

    added while loop

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -9,4 +9,8 @@ ip_address=$1
 port=8080
 microk8s.helm install --name $name --namespace $namespace -f $chart_values $helm_chart
 printf $(microk8s.kubectl get secret --namespace $namespace $name -o jsonpath="{.data.${name}-admin-password}" | base64 --decode);echo
+while `microk8s.kubectl get pod -n jenkins | grep jenkins | awk '{print $4}'` == 0; do
+    sleep 5
+done
+
 microk8s.kubectl port-forward $svc_name -n $name --address $ip_address 8080 &
